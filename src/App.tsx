f17ba689d46c8cb16245ad1665971e76f3fb59ef
Andreas <andreas.just.petersen@gmail.com>
2023-01-14 00:15:20 +0100

    Init

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import init from 'wasm-lib';
+import { add } from 'wasm-lib';
 
 function App() {
+
+    useEffect(() => {
+	init().then(() => {
+	    console.log(add(5, 6))
+	    
+	})
+    }, [])
+
   return (
     <div className="App">
       <header className="App-header">
