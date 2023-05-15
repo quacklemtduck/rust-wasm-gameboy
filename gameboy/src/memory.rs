3934f74e2e96112177cd754b28257874fa734297
Andreas <andreas.just.petersen@gmail.com>
2023-05-15 13:27:45 +0200

    Last changes

--- gameboy/src/memory.rs
+++ gameboy/src/memory.rs
@@ -189,61 +189,6 @@ impl Memory {
         self.write(0xffff, 0x00);
     }
 
-    // pub fn simulate_bootloader(&mut self) {
-    //     self.write(0xff04, 0xab);
-    //     self.write(0xff05, 0x00);
-    //     self.write(0xff06, 0x00);
-    //     self.write(0xff07, 0xf8);
-    //     self.write(0xff10, 0x80);
-    //     self.write(0xff11, 0x80);
-    //     self.write(0xff12, 0xf3);
-    //     self.write(0xff13, 0xc1);
-    //     self.write(0xff14, 0x87);
-    //     self.write(0xff16, 0x3f);
-    //     self.write(0xff17, 0x00);
-    //     self.write(0xff19, 0xbf);
-    //     self.write(0xff1a, 0x7f);
-    //     self.write(0xff1b, 0xff);
-    //     self.write(0xff1c, 0x9f);
-    //     self.write(0xff1e, 0xbf);
-    //     self.write(0xff20, 0xff);
-    //     self.write(0xff21, 0x00);
-    //     self.write(0xff22, 0x00);
-    //     self.write(0xff23, 0xbf);
-    //     self.write(0xff24, 0x77);
-    //     self.write(0xff25, 0xf3);
-    //     self.write(0xff26, 0x80);
-    //     self.write(0xff40, 0x91);
-    //     self.write(0xff41, 0x82);
-    //     self.write(0xff42, 0x00);
-    //     self.write(0xff43, 0x00);
-    //     self.write(0xff44, 0x8f);
-    //     self.write(0xff45, 0x00);
-    //     self.write(0xff47, 0xfc);
-    //     self.write(0xff48, 0xff);
-    //     self.write(0xff49, 0xff);
-
-    //     self.write(0xff51, 0xFF);
-    //     self.write(0xff52, 0xFF);
-    //     self.write(0xff53, 0xFF);
-    //     self.write(0xff54, 0xFF);
-    //     self.write(0xff55, 0xFF);
-    //     self.write(0xff51, 0xFF);
-    //     self.write(0xff68, 0xFF);
-    //     self.write(0xff69, 0xFF);
-    //     self.write(0xff6a, 0xFF);
-    //     self.write(0xff6b, 0xFF);
-
-
-
-    //     self.write(0xff4a, 0x00);
-    //     self.write(0xff4b, 0x00);
-    //     self.write(0xff50, 0x01);
-    //     self.write(0xfffb, 0x01);
-    //     self.write(0xfffc, 0x2e);
-    //     self.write(0xfffd, 0x00);
-    //     self.write(0xffff, 0x00);
-    // }
 
     pub fn set_joypad_state(&mut self, up: i32, right: i32, down: i32, left: i32, a: i32, b: i32, select: i32, start: i32) {
         let request_interrupt = self.joypad.set_joypad_state(up, right, down, left, a, b, select, start);
